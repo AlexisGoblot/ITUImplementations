e5bc6ddb9076438ab600b53e44dd54082b4a1aa2
ThomasLebreton <thomas.lebreton35580@gmail.com>
2021-05-29 14:28:01 +0200

    Commenting the code in English
    Adding some explanations for the ITU 2402 model

--- models/itu2402.py
+++ models/itu2402.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import PchipInterpolator
 import pandas as pd
@@ -6,16 +5,22 @@ import tkinter.ttk
 
 from models.base_classes import ITU, Model
 
-q = np.linspace(0, 1, 200)
 
 # definition of the urban templates:
+
+# Number of buildings used in the vector
+q = np.linspace(0, 1, 200)
+
+
+# London building definition, using points in the cdf presented in the ITU 2402
 cdfHLondon = np.array(
     [[0, 0], [8, 1e-3], [15, 0.05], [18, 0.09], [20, 0.3], [25, 0.5], [40, 0.9], [60, 0.93], [85, 0.97],
      [150, 0.9701],
      [151, 0.98], [170, 1]])
-cdfD1London = np.array([[0, 0], [10, 0.5], [25, 0.8], [50, 0.91], [100, 0.9 + 0.075], [150, 0.98], [350, 1]])
+cdfD1London = np.array([[0, 0], [10, 0.5], [25, 0.8], [50, 0.91], [100, 0.975], [150, 0.98], [350, 1]])
 cdfD2London = np.array(
     [[0, 0], [10, 1e-3], [25, 0.1], [50, 0.35], [90, 0.8], [100, 0.85], [150, 0.95], [200, 0.98], [350, 1]])
+
 # creates interpolators
 IPH = PchipInterpolator(cdfHLondon[:, 1], cdfHLondon[:, 0])
 IPD1 = PchipInterpolator(cdfD1London[:, 1], cdfD1London[:, 0])
@@ -24,9 +29,10 @@ IPD2 = PchipInterpolator(cdfD2London[:, 1], cdfD2London[:, 0])
 sdhL = pd.Series(index=q, data=IPH(q))
 sd2L = pd.Series(index=q, data=IPD2(q))
 sd1L = pd.Series(index=q, data=IPD1(q))
+
 urban_template_london = {'Db1': sd1L.values, 'Db2': sd2L.values, 'Hb': sdhL.values}
 
-# Melbourne
+# Melbourne building definition, using points in the cdf presented in the ITU 2402
 
 cdfDb1Melbourne = np.array(
     [[0, 0], [8.3, 0.2], [19, 0.5], [45.5, 0.8], [82, 0.9], [100, 0.95], [150, 0.972], [200, 0.985], [350, 1]])
@@ -56,7 +62,7 @@ urban_templates = {"london": urban_template_london, "melbourne": urban_template_
 def compute_models(N: int = 10000, f: float = 30.0, Hs: float = 5.0, theta: float = 40.0,
                    city: str = "london") -> dict:
     """
-    First model described in the ITU 2108 description.
+    Model described in the ITU 2402 description.
 
     Parameters
     ----------
@@ -82,9 +88,8 @@ def compute_models(N: int = 10000, f: float = 30.0, Hs: float = 5.0, theta: floa
 
     """
     theta = np.array([theta])
-    # definition des batiments de londre
 
-    # constantes de la recommandation
+    # Parameters defined in page 8 of ITU 2402
     Kdr = 0.5
     Kdh = 1.5
     Khc = 0.3
@@ -92,13 +97,13 @@ def compute_models(N: int = 10000, f: float = 30.0, Hs: float = 5.0, theta: floa
     Krs = 15
     Krm = 8
 
-    # Generation des batiments de la ville, section 4.4
+    # Generation of the city, section 4.4
 
     Db1_vect = urban_templates[city]['Db1']
     Db2_vect = urban_templates[city]['Db2']
     Hb_vect = urban_templates[city]['Hb']
 
-    # Section 5.4.1
+    # Generating the buildings for the simulation, section 5.4.1
 
     Db1 = np.quantile(Db1_vect, np.random.rand(N), interpolation='higher')
     Db2 = np.quantile(Db2_vect, np.random.rand(N), interpolation='higher')
@@ -112,7 +117,7 @@ def compute_models(N: int = 10000, f: float = 30.0, Hs: float = 5.0, theta: floa
     Dr23 = np.quantile(Db1_vect, pr23, interpolation='higher')
     Dr34 = np.quantile(Db1_vect, pr34, interpolation='higher')
 
-    # Section 5.4.2
+    # Generating the roof heights, section 5.4.2
 
     Hc = np.quantile(Hb_vect, Khc, interpolation='higher')
 
@@ -130,9 +135,9 @@ def compute_models(N: int = 10000, f: float = 30.0, Hs: float = 5.0, theta: floa
     Hb3s = np.quantile(Hb_vect, np.random.rand(N), interpolation='higher') - Hs
     Hb4s = np.quantile(Hb_vect, np.random.rand(N), interpolation='higher') - Hs
 
-    # Section 5.5
+    # Calculation of the diffraction loss, section 5.5
 
-    lambd = 0.3 / f  # avec f en GHz
+    lambd = 0.3 / f
 
     Hrs1 = hray(0, Db1, theta)
     nu1 = nu_calcul(Db1, Hb1, Hrs1, lambd, Hs)
@@ -144,80 +149,83 @@ def compute_models(N: int = 10000, f: float = 30.0, Hs: float = 5.0, theta: floa
 
     Ld2 = J(nu2)
 
-    # Ld = Ld1 + Ld2
-
     Ld = 10 * np.log10((10 ** (0.1 * Ld1) + 10 ** (0.1 * Ld2)) * (1 + Ld1 + Ld2) / (2 + Ld1 + Ld2))
 
-    # Section 5.6
+    # Calculation of the reflexion loss, section 5.6
 
-    # On reprend les vrais hauteurs de batiments
+    # Here we take back the real heights of the buildings
     Hb1s = Hb1 - Hs
     Hb2s = Hb2 - Hs
 
+    # Here we calculate the ray height
     Hrs3_1 = hray(Hrs1, Dr13, theta)
     Hrs3_2 = hray(Hrs2, Dr23, theta)
     Hrs4_1 = hray(Hrs3_1, Dr34, theta)
     Hrs4_2 = hray(Hrs3_2, Dr34, theta)
 
-    # On initialise la matrice Nr
+    # Initialization of the number of reflexion
+    # The following calculations refers to the figure 5 of ITU 2402
     Nr = np.zeros([N, 1])
 
-    Nr[Hrs1 < Hb1s[:, None]] += 1
-    Nr[(Nr == 1) & (Hrs3_1 < Hb3s[:, None])] += 1
-    Nr[(Nr == 2) & (Hrs4_1 < Hb4s[:, None])] = 0
+    # First case, the ray is reflecting of the building B1
+    Nr[Hrs1 < Hb1s[:,None]] += 1
+    
+    # Then we check if there is a reflexion on the buildings B3 or B4
+    Nr[(Nr == 1) & (Hrs3_1 < Hb3s[:,None])] += 1
+    Nr[(Nr == 2) & (Hrs4_1 < Hb4s[:,None])] = 0
 
-    Nr[Nr != 0] += 5  # astuce pour eviter de traiter les cas qui ont ete reflechis
-    # sur le batiment 1 dans la partie reflexion sur le batiment 2
 
-    Nr[(Nr == 0) & (Hrs2 < Hb2s[:, None])] += 1
-    Nr[(Nr == 1) & (Hrs3_2 < Hb3s[:, None])] += 1
-    Nr[(Nr == 2) & (Hrs4_2 < Hb4s[:, None])] = 0
+    # A trick to not use the cases already used previously
+    # For exemple, if a ray is reflected on the B1 but not on the B2, B3 and B4,
+    # its value in the vector Nr is 1, but if the condition Hrs3_2 < Hb3s
+    # is true, the value will be increment, and we don't want that
+    # so we add 5 to all the cases we already processed to not modify them accidently
+    Nr[Nr != 0] += 5  
 
-    Nr[Nr > 3] -= 5  # on remet les reflexions comme il faut
+    # Then we process to the second case, the ray is reflecting of the building B2
+    Nr[(Nr == 0) & (Hrs2 < Hb2s[:,None])] += 1
+    Nr[(Nr == 1) & (Hrs3_2 < Hb3s[:,None])] += 1
+    Nr[(Nr == 2) & (Hrs4_2 < Hb4s[:,None])] = 0
+
+    # At the end, we put back the real number of reflexion
+    Nr[Nr > 3] -= 5
 
     Llof = Krm - Krs * np.log10(f / Krc)
     Lr = 10 * np.log10(10 ** (0.1 * Krm) + 10 ** (0.1 * Llof))
 
-    # Section 5.7
+    # Calculation of the clutter loss, section 5.7
 
     p = np.random.rand(N)
     # In order to randomize Reflexion coeff
     coeff = (-np.log(1 - p)) ** 0.5 / 0.833
+    
     Lc = Ld.copy()
+    
     Lc[Nr == 1] = -10 * np.log10(
         10 ** (0.1 * (-Ld[Nr == 1])) + 10 ** (0.1 * (-1 * Lr * np.resize(coeff, [N, 1])[:, :][Nr == 1])))
     Lc[Nr == 2] = -10 * np.log10(
         10 ** (0.1 * (-Ld[Nr == 2])) + 10 ** (0.1 * (-2 * Lr * np.resize(coeff, [N, 1])[:, :][Nr == 2])))
+    
+    # Here we isolate the reflexnio loss, in order to be able to draw it
     Lossr = np.zeros(Lc.shape)
     Lossr[Nr == 0] = 0
     Lossr[Nr == 1] = -10 * np.log10(10 ** (0.1 * (-1 * Lr * np.resize(coeff, [N, 1])[:, :][Nr == 1])))
     Lossr[Nr == 2] = -10 * np.log10(10 ** (0.1 * (-2 * Lr * np.resize(coeff, [N, 1])[:, :][Nr == 2])))
 
-    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple',
-              'tab:brown', 'tab:pink', 'tab:grey', 'tab:olive', 'tab:cyan']
-
     values = {}
-    # affichage des clutter loss (Lc)
+    # Clutter loss display (Lc)
     tmp_x = np.sort(Lc[:, 0])
     tmp_y = np.array(range(N)) / float(N)
     values["clutter loss"] = {"x": tmp_x, "y": tmp_y, "label": f"{str(theta[0])} degrés"}
-    # plt.plot(tmp_x, tmp_y, colors[0], label=f"{str(theta[0])} degrés clutter loss")
 
-    # affichage des pertes de diffraction (Ld)
+    # Diffraction loss display (Ld)
     tmp_x = np.sort(Ld[:, 0])
-    # plt.plot(tmp_x, tmp_y, colors[0], linestyle='--', label=f"{str(theta[0])} degrés perte diffraction")
     values["diffraction loss"] = {"x": tmp_x, "y": tmp_y, "label": f"{str(theta[0])} degrés"}
 
-    # affichage des pertes de reflexion (Lr)
+    # Reflexion loss display (Lr)
     tmp_x = np.sort(Lossr[:, 0])
-    # plt.plot(tmp_x, tmp_y, colors[0], linestyle=':', label=f"{str(theta[0])} degrés perte de réflexion")
     values["reflection loss"] = {"x": tmp_x, "y": tmp_y, "label": f"{str(theta[0])} degrés"}
 
-    # plt.xlabel("dB")
-    # plt.ylabel("percent of locations")
-    # plt.title("titre")
-    # plt.legend()
-    # plt.show()
     return values
 
 
@@ -285,8 +293,7 @@ class ITU2402(ITU):
         return values["x"], values["y"], values["label"]
 
 
-# Fonctions
-# Fonction Hray (5.4.3)
+
 def hray(Hrsx, Dxy, angle):
     """
     Parameters
@@ -304,7 +311,7 @@ def hray(Hrsx, Dxy, angle):
     Formula (10) of ITU-R P-2402
     """
     angle = (angle * np.pi) / 180
-    return Hrsx + Dxy[:, None] * np.tan(angle[None, :])
+    return Hrsx + Dxy[:,None] * np.tan(angle[None,:])
 
 
 def nu_calcul(Db, Hb, Hrs, Lambda, Hs=5):
@@ -326,15 +333,14 @@ def nu_calcul(Db, Hb, Hrs, Lambda, Hs=5):
     Formula (12) of ITU-R P-2402
 
     """
-    Ro = (Hb[:, None] - Hs - Hrs) / (np.sqrt(Db[:, None] ** 2 + Hrs ** 2))
-    ho = Ro * Db[:, None]
-    do = np.sqrt(Db[:, None] ** 2 + Hrs ** 2) + Ro * Hrs
+    Ro = (Hb[:,None] - Hs - Hrs) / (np.sqrt(Db[:,None] ** 2 + Hrs ** 2))
+    ho = Ro * Db[:,None]
+    do = np.sqrt(Db[:,None] ** 2 + Hrs ** 2) + Ro * Hrs
 
     nu = 2 * np.sqrt((np.sqrt(do ** 2 + ho ** 2) - do) / Lambda) * np.sign(ho)
     return nu
 
 
-# Fonction J (5.5 equation 11)
 def J(nu):
     """
     Parameters
